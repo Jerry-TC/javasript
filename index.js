d0c1d94fe46782ff901abe68cd4f4f412bd8f3ad
Jerry_TC <akamsjeremiah@gmail.com>
2024-09-13 12:17:52 +0100

    new commit javascript class

---
+++ index.js
@@ -0,0 +1,66 @@
+
+
+//console.log(alert('Enter your name') ) //This code rrequests visitors name
+
+/*  This is a 
+1
+    multiline 
+
+    comment
+
+*/
+
+// Fname = prompt() ;
+
+
+let fname = 'Aaron' ;
+let lname = 'John' ;
+let sage = 10;
+let sname = 'william';
+fname = 'James';
+const xname = 'Jerry';
+
+
+
+const company = 'Nedine Waters';
+
+console.log(fname + lname + sname)
+console.log( xname)
+console.log(company)
+let num2 = 14;
+let num1 = 2;
+let variable = num1 < num2
+
+/*console.log(alert(variable))*/
+let vany = typeof(true)
+console.log(vany)
+
+fname = 'saron'
+console.log(9007199254740991+1)
+console.log(9007199254740991+2)
+let profile = {
+    first_name: 'John',
+    last_name: 'smith',
+    age: 20,         
+}
+
+console.log('my name is' + '  ' + profile.first_name)
+let x = 10
+let y = 13
+
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x%y)
+console.log(x/y)
+console.log(x**y)
+console.log(x += 5)
+console.log(x == 5)
+let gname = 'John'
+console.log(gname !== 'james')
+console.log(x>y)
+console.log(x<y)
+
+let title = 'Things fall Apart';
+
+console.log (title.length)
